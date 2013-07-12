190848d886fc89c163dcb54a6b28328914fd01e2
Ubuntu <ubuntu@ip-10-164-92-58.ec2.internal>
2013-07-12 21:27:56 +0000

    iRead from index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var buffer = new Buffer(256);
+
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    buffer.write(data, "utf-8");
+  });
+
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
